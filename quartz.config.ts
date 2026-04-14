978f6010db7980a5c494c05b6a0c7f77cbfd20b9
Octdecimal <572neet989@gmail.com>
2026-04-14 21:42:51 +0800

    Remove page title suffix from configuration

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "御門トクテル",
-    pageTitleSuffix: "Mikado Tokuteru",
+    pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
